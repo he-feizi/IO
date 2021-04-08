052ef658f356c4101991102d5d239a71444182ed
he-feizi <wokefei@mail.ustc.edu.cn>
2021-04-08 12:01:25 +0800

    BIO阻塞模型

---
+++ src/com/feifei/socket_demo/Client.java
@@ -0,0 +1,58 @@
+package com.feifei.socket_demo;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * @ClassName Client
+ * @date 2021-04-08 11:39
+ * @Version 1.0
+ * @Description 客户端端口
+ */
+public class Client {
+    public static void main(String[] args) {
+        final String DEFAULT_SERVER_HOST = "127.0.0.1";
+        final int DEFAULT_SERVER_PORT = 8888;
+        Socket socket = null;
+        BufferedWriter writer = null;
+
+        try {
+            // 创建socket
+            socket = new Socket(DEFAULT_SERVER_HOST, DEFAULT_SERVER_PORT);
+
+            // 创建IO流
+            BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(socket.getInputStream())
+            );
+             writer = new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())
+            );
+
+            // 等待用户输入信息
+            BufferedReader consoleReader = new BufferedReader(
+                    new InputStreamReader(System.in)
+            );
+            String input = consoleReader.readLine();
+
+            // 发送消息给服务器
+            writer.write(input + "\n");
+            writer.flush();
+
+            // 读取服务器返回的消息
+            String msg = reader.readLine();
+            System.out.println(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (writer != null) {
+                try {
+                    writer.close();
+                    System.out.println("关闭socket");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+}
---
+++ src/com/feifei/socket_demo/Server.java
@@ -0,0 +1,59 @@
+package com.feifei.socket_demo;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * @ClassName Server
+ * @date 2021-04-08 11:25
+ * @Version 1.0
+ * @Description 服务器端口
+ */
+public class Server {
+
+    public static void main(String[] args) {
+        final int DEFAULT_PORT = 8888;
+        ServerSocket serverSocket = null;
+
+        try {
+            // 绑定监听端口
+            serverSocket = new ServerSocket(DEFAULT_PORT);
+            System.out.println("启动服务器，监听端口" + DEFAULT_PORT);
+
+            while (true) {
+                // 等待客户端连接，阻塞式等待
+                Socket socket = serverSocket.accept();
+                System.out.println("客户端【" + socket.getPort() + "】已连接");
+                BufferedReader reader = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream())
+                );
+                BufferedWriter writer = new BufferedWriter(
+                        new OutputStreamWriter(socket.getOutputStream())
+                );
+
+                // 读取客户端发送的消息
+                String msg = reader.readLine();
+                if (msg != null) {
+                    System.out.println("客户端【" + socket.getPort() + "】：" + msg);
+
+                    // 回复客户端发送的消息
+                    writer.write("服务器：" + msg + "\n");
+                    writer.flush();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (serverSocket != null) {
+                try {
+                    // 关闭 serverSocket
+                    serverSocket.close();
+                    System.out.println("关闭serverSocket");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
